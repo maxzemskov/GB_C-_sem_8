287fa26dda1a19e5a76e43825ce5c8dc316bc55a
mzemskov <mzemskov@dimax.digital>
2022-12-21 10:44:50 +0300

    task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,52 @@
+﻿	void NewMatrix(int[,] matrix)
+	{
+	    for (int i = 0; i < matrix.GetLength(0); i++)
+	    {
+	        for (int j = 0; j < matrix.GetLength(1); j++)
+	            matrix[i, j] = new Random().Next(-10, 11);
+	    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+
+void SortMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+NewMatrix(matrix);
+Console.WriteLine("Начальный двумерный массив: ");
+PrintMatrix(matrix);
+Console.WriteLine();
+SortMatrix(matrix);
+Console.WriteLine("Конечный двумерный массив: ");
+PrintMatrix(matrix);
