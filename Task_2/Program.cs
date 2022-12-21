f487bb5e9cef2cb7b3caf9f2c1f3068e0e5b8873
mzemskov <mzemskov@dimax.digital>
2022-12-21 14:26:37 +0300

    task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,52 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 11); 
+    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+
+int MinSumMatrix(int[,] matrix)
+{
+    int minRow = 10000;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sumRow = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sumRow += matrix[i, j];
+        }
+        if (minRow > sumRow)
+            minRow = sumRow;
+    }
+    return minRow;
+}
+
+
+Console.Write("Введите размер прямоугольного двумерного массива: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+while (size[0] == size[1])
+{
+    Console.Write("Вы квадратный массив!\nВведите размер прямоугольного двумерного массива: ");
+    size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+}
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Начальный массив: ");
+PrintMatrix(matrix);
+Console.WriteLine();
+Console.WriteLine($"Минимальная сумма = {MinSumMatrix(matrix)}");
